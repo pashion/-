fafdcb64a72dbb02872d215977dd6921f9e701b9
Mvp <http://192.168.35.166:3000/Mvp/laravel-shop.git>
2017-06-27 20:03:28 +0800

    修正路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,104 +1,86 @@
 <?php
 
+	//前台登录路由
+	Route::get('/', function () {
+
+	    return view('web.index');
+	});
 
 
-//登录页面和操作
-Route::controller('/admins', 'AdminLoginController');
+
+	//后台登录页面和操作路由
+	Route::controller('/admins', 'AdminLoginController');
 
 //后台路由群，由中间件控制用户访问是否登录
 Route::group(['middleware'=>'adminLogin'], function () {
 	//后台用户管理
-	Route::controller('/admin/user','UserController');	
+	Route::controller('/admin/user','UserController');
+	//权限管理路由	
 	Route::controller('/admin/roles','RolesController');	
 	Route::controller('/admin/permissions','PermissionsController');
 	Route::controller('/admin/users','UsersController');
+
+
+
+	Route::resource('goods', 'GoogsController');
+	Route::resource('frenship','FrenshipController');
+	Route::get('frenship/{id}/delete','FrenshipController@destroy');
+	Route::resource('talking','TalkingController');
+	Route::get('talking/{id}/delete','TalkingController@destroy');
+	Route::resource('order','OrderController');
+
+
+
+	//添加子分类页面路由
+	Route::get('/SecoundType/increase','SecoundTypeController@increase');
+	//添加子分类处理路由
+	Route::post('/SecoundType/insert','SecoundTypeController@insert');
+	//分类ajax页面路由
+	Route::get('/SecoundType/ajax','SecoundTypeController@ajax');
+	//接收ajax请求并反馈路由
+	Route::get('/SecoundType/getmsg','SecoundTypeController@getmsg');
+	//分类资源路由
+	Route::resource('/SecoundType', 'SecoundTypeController');
+
+	//轮播图资源路由
+	Route::resource('/Wheel','WheelController');
+	//触发ajax
+	Route::post('/Wheel/sort','WheelController@sort');
+
+	//文章模块路由
+	Route::resource('Article','ArticleController');
+	//文章封面图ajax路由
+	Route::post('Article/ajax','ArticleController@ajax');
+
+
+	//商品资源路由
+	Route::resource('goods', 'GoogsController');
+
+
+	//商品规格
+	Route::resource('goodsSpec', 'GoodsSpecController');
+
+
+	//商品分类查询路由
+	Route::get('goodsType', 'GoodsTypeController@getKindType');
+	Route::get('goodsgetstyle', 'GoodsTypeController@getGoodsStyle');
+
+	Route::post('goodsgetstyles', 'GoodsTypeController@getStyle');
+	//商品文件上传
+	Route::post('goods/file/upload', 'GoodsFileController@uploadGoodsFile');
+	//商品图片取消
+	Route::get('goods/file/upload', 'GoodsFileController@canclePic');
+	//商品图片缩略图获取路由
+	Route::get('goods/file/reducepic', 'GoodsFileController@reduce');
+
+
 	
 });
 
 
-Route::resource('goods', 'GoogsController');
-Route::resource('frenship','FrenshipController');
-Route::get('frenship/{id}/delete','FrenshipController@destroy');
-Route::resource('talking','TalkingController');
-Route::get('talking/{id}/delete','TalkingController@destroy');
-Route::resource('order','OrderController');
-
-Route::get('/', function () {
-
-<<<<<<< HEAD
-    return view('web.index');
-=======
-    return view('welcome');
-
->>>>>>> 2d44165622a24110e9575169906997b8a2705554
-});
-//测试图形缩放类
-Route::get('/', function(){
-    $img = Image::make('../public/uploads/Wheel_Sfm.jpg')->resize(300, 200);
-    return $img->response('jpg');
-});
-
-//添加子分类页面路由
-Route::get('/SecoundType/increase','SecoundTypeController@increase');
-//添加子分类处理路由
-Route::post('/SecoundType/insert','SecoundTypeController@insert');
-//分类ajax页面路由
-Route::get('/SecoundType/ajax','SecoundTypeController@ajax');
-//接收ajax请求并反馈路由
-Route::get('/SecoundType/getmsg','SecoundTypeController@getmsg');
-//分类资源路由
-Route::resource('/SecoundType', 'SecoundTypeController');
-
-//轮播图资源路由
-Route::resource('/Wheel','WheelController');
-//触发ajax
-Route::post('/Wheel/sort','WheelController@sort');
-
-//文章模块路由
-Route::resource('Article','ArticleController');
-//文章封面图ajax路由
-Route::post('Article/ajax','ArticleController@ajax');
-
-
-<<<<<<< HEAD
-=======
-//商品资源路由
-Route::resource('goods', 'GoogsController');
-
-
-//商品规格
-Route::resource('goodsSpec', 'GoodsSpecController');
-
-
-//商品分类查询路由
-Route::get('goodsType', 'GoodsTypeController@getKindType');
-Route::get('goodsgetstyle', 'GoodsTypeController@getGoodsStyle');
-
-Route::post('goodsgetstyles', 'GoodsTypeController@getStyle');
-//商品文件上传
-Route::post('goods/file/upload', 'GoodsFileController@uploadGoodsFile');
-//商品图片取消
-Route::get('goods/file/upload', 'GoodsFileController@canclePic');
-//商品图片缩略图获取路由
-Route::get('goods/file/reducepic', 'GoodsFileController@reduce');
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 2d44165622a24110e9575169906997b8a2705554
