3ec056ec30887780bcc99ab8194ee2b563f5113c
Confucianer <1020383441@qq.com>
2017-06-29 14:46:27 +0800

    分类、轮播图、文章模块+数据库修改

--- database/migrations/2017_06_29_144212_create_wheel_table.php
+++ database/migrations/2017_06_29_144212_create_wheel_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSecondTypeTable extends Migration
+class CreateWheelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,12 @@ class CreateSecondTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('second_type', function (Blueprint $table) {
+        Schema::create('wheel', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');//类名
-            $table->integer('tid');//父类id
-            $table->string('path');//关联路径
-            $table->increments('sort')//类型顺序
+            $table->string('picname',200);//轮播图描述
+            $table->string('picurl',200);//轮播图名字
+            $table->increments('sort');//轮播图顺序
+            $table->string('path',255);//轮播图路径
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateSecondTypeTable extends Migration
      */
     public function down()
     {
-        Schema::drop('second_type');
+        Schema::drop('wheel');
     }
 }
